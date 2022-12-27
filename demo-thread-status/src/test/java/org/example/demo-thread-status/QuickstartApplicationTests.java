de99c2f1e8f7057e94bd7c1a8d746e502c6b3ce5
jianhongli <qunar.jianhong.li@gmail.com>
2022-12-27 14:47:34 +0800

    ADD: init file

---
+++ demo-thread-status/src/test/java/org/example/demo-thread-status/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo-thread-status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
